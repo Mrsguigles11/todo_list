d664be9f0b1aa029b0d1bfbcc84b232e25b6c1f7
Harry <harry_woodgate11@hotmail.co.uk>
2024-12-09 10:15:00 +0000

    Add a dom module.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
 import "./styles.css";
+
+(function () {
+    const domCache = {
+        newTaskButton : document.querySelector("#new_task_button"),
+        newListButton : document.querySelector("#new_list_button"),
+    }
+
+    const bindEvents = function () {
+        domCache.newListButton.addEventListener('click', () => {
+            const newListName = prompt("Enter new list name");
+            listManager.publishList(newListName);
+        })
+    }
+
+    bindEvents();
+
+    return{domCache};
+})();
+
+// class listManager {
+//     constructor(list) {
+//         this.list = list;
+//     }
+
+//     publishList(list) {
+//         this.list = list
+//     }
+
+// }
